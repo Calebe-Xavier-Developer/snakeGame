5b2165efa81bf1978d59c5b323d60c710f39f320
Calebe-Xavier-Developer <57916053+Calebe-Xavier-Developer@users.noreply.github.com>
2021-06-07 07:33:31 -0300

    Fruta com localização aleatoria criada

--- script.js
+++ script.js
@@ -7,18 +7,27 @@ snake[0] = {
   y: 8 * box
 };
 let direction = 'right';
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+};
 
 function criarBG() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
-}
+};
 
 function criarCobrinha() {
   for (i = 0; i < snake.length; i++) {
     context.fillStyle = 'green';
     context.fillRect(snake[i].x, snake[i].y, box, box);
-  }
-}
+  };
+};
+
+function drawFood() {
+  context.fillStyle = 'red';
+  context.fillRect(food.x, food.y, box, box);
+};
 
 document.addEventListener('keydown', update);
 
@@ -27,7 +36,7 @@ function update(event) {
   if (event.keyCode == 38 && direction != 'down') direction = 'up';
   if (event.keyCode == 39 && direction != 'left') direction = 'right';
   if (event.keyCode == 40 && direction != 'up') direction = 'down';
-}
+};
 
 function iniciarJogo() {
   if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
@@ -37,6 +46,8 @@ function iniciarJogo() {
 
   criarBG();
   criarCobrinha();
+  drawFood();
+
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
@@ -50,9 +61,9 @@ function iniciarJogo() {
   let newHead = {
     x: snakeX,
     y: snakeY
-  }
+  };
 
   snake.unshift(newHead);
-}
+};
 
 let jogo = setInterval(iniciarJogo, 100);
